48f0cf6470d9a76012acfe3bf2ac7a9aa52996c2
EdilsonRogerioCuambe <edilson@aluno.unilab.edu.br>
2024-01-13 03:41:04 -0300

    Error Boundary

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,8 @@ import Navbar from '@/components/navbar'
 import Footer from '@/components/footer'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
+import { ErrorBoundary } from 'next/dist/client/components/error-boundary'
+import ErrorComponent from '@/components/err'
 
 const mono = Mono({ subsets: ['latin'] })
 
@@ -21,10 +23,12 @@ export default function RootLayout({
   return (
     <html lang="pt">
       <body className={`${mono.className} bg-[#202024] text-[#c4c4cc]`}>
-        <Navbar />
-        {children}
-        <Footer />
-        <ToastContainer />
+        <ErrorBoundary errorComponent={ErrorComponent}>
+          <Navbar />
+          {children}
+          <Footer />
+          <ToastContainer />
+        </ErrorBoundary>
       </body>
     </html>
   )
---
+++ src/utils/ErrorBoundary.tsx
@@ -0,0 +1,51 @@
+import React, { ErrorInfo, ReactNode } from 'react'
+
+type ErrorBoundaryProps = {
+  children: ReactNode
+  errorComponent: ReactNode // Assumindo que errorComponent é um ReactNode
+}
+
+type ErrorBoundaryState = {
+  hasError: boolean
+}
+
+class ErrorBoundary extends React.Component<
+  ErrorBoundaryProps,
+  ErrorBoundaryState
+> {
+  constructor(props: ErrorBoundaryProps) {
+    super(props)
+    this.state = { hasError: false }
+  }
+
+  static getDerivedStateFromError(_: Error): ErrorBoundaryState {
+    // Update state so the next render will show the fallback UI
+    return { hasError: true }
+  }
+
+  componentDidCatch(error: Error, errorInfo: ErrorInfo): void {
+    // You can use your own error logging service here
+    console.log({ error, errorInfo })
+  }
+
+  render(): ReactNode {
+    if (this.state.hasError) {
+      // Custom fallback UI
+      return (
+        <div>
+          <h2>Oops, there is an error!</h2>
+          <button
+            type="button"
+            onClick={() => this.setState({ hasError: false })}
+          >
+            Try again?
+          </button>
+        </div>
+      )
+    }
+
+    return this.props.children
+  }
+}
+
+export default ErrorBoundary
