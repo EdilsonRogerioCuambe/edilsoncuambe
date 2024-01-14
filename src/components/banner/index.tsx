22176856187606906f71e2f385e52fe780808fce
EdilsonRogerioCuambe <edilson@aluno.unilab.edu.br>
2024-01-14 04:03:36 -0300

    update banner

--- src/components/banner/index.tsx
+++ src/components/banner/index.tsx
@@ -1,8 +1,10 @@
 export default function Banner({ title }: { title: string }) {
   return (
-    <div className="px-4 py-32 bg-[#323238] mx-auto">
-      <div className="text-center justify-center items-center max-w-7xl mx-auto">
-        <h1 className="text-5xl lg:text-7xl leading-snug font-bold">{title}</h1>
+    <div className="px-4 md:px-8 py-20 md:py-32 bg-[#323238] mx-auto">
+      <div className="text-center justify-center items-center max-w-4xl md:max-w-6xl lg:max-w-7xl mx-auto">
+        <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl 2xl:text-8xl leading-snug font-bold">
+          {title}
+        </h1>
       </div>
     </div>
   )
